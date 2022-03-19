67b1d1a9426352e28b1f0a6e6b8362a44ebade2f
mertcancetinok <69702906+mertcancetinok@users.noreply.github.com>
2022-03-19 21:36:07 +0300

    core layer implementation

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services;
+
+public interface IService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    IQueryable<T> Where(Expression<Func<T, bool>> expression);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+    Task<T> AddAsync(T entity);
+    Task<T> AddRangeAsync(IEnumerable<T> entity);
+    Task UpdateAsync(T entity);
+    Task RemoveAsync(T entity);
+    Task RemoveRangeAsync(IEnumerable<T> entity);
+}
